7eb1bb6f1db44d64a3469192913b48d0e1623226
carlo briganti <60385209+calrloco@users.noreply.github.com>
2024-04-28 16:58:45 +0200

    changes

---
+++ src/components/DirectionAwareHoverDemo.tsx
@@ -0,0 +1,17 @@
+import { useRef, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { cn } from "@/utils/cn";
+import { DirectionAwareHover } from "../ui/direction-aware-hover";
+
+export function DirectionAwareHoverDemo() {
+  const imageUrl =
+    "https://images.unsplash.com/photo-1663765970236-f2acfde22237?q=80&w=3542&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  return (
+    <div className="h-[40rem] relative  flex items-center justify-center">
+      <DirectionAwareHover imageUrl={imageUrl}>
+        <p className="font-bold text-xl">In the mountains</p>
+        <p className="font-normal text-sm">$1299 / night</p>
+      </DirectionAwareHover>
+    </div>
+  );
+}
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,12 +15,12 @@ export default function Hero() {
             Make your websites look 10x awesome
           </p>
         </div>
-        <div className="grid grid-rows-2 gap-3 absolute left-1/2 pl-10">
+        <div className="grid grid-rows-2 gap-3 absolute left-1/2 pl-5">
           <div className="flex gap-3 items-center">
             <MeteorsDemo />
             <GlowingStarsBackgroundCardPreview />
           </div>
-          <div className="-bottom-[90%] row-start-2">
+          <div className="row-start-2">
             <TextRevealCardPreview />
           </div>
         </div>
