913844db1311a4232d71f1bd7d25bb42983897b2
carlo briganti <60385209+calrloco@users.noreply.github.com>
2024-05-01 09:54:52 +0200

    redme

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # Astro Starter Template with Aceternity UI
 
-This starter template is tailored for developers looking to integrate the Aceternity UI component library, originally designed for Next.js, into Astro projects. It offers a straightforward method to incorporate Aceternity's refined UI components directly into your Astro applications without the need for additional configurations like React import statements.
+This starter template is tailored for developers looking to integrate the Aceternity UI component library, originally designed for Next.js, into Astro projects.
 
 ## Features
 
 - **Aceternity UI Integration**: Pre-integrated with the Aceternity UI component library for immediate use.
-- **Simplified Setup**: Components and demo examples included in the project, allowing for quick and easy customization.
+- **Simplified Setup**: Most of Components and demo examples included in the project, allowing for quick and easy customization.
 
 ## Prerequisites
 
@@ -19,12 +19,16 @@ You can create a new project using this template directly from the repository:
    Visit https://github.com/calrloco/astro-aceternity and use the "Use this template" button to create a new repository based on this starter template.
 
 2. **Install Dependencies and Start Development**
-   ```bash
-   npm install && npm run astro dev
-   ```
 
-   Access your project at http://localhost:4321/
+```shell
+npm install
+```
 
+```shell
+ npm run dev
+```
+
+Access your project at http://localhost:4321/
 
 ## Contributing
 
---
+++ src/components/BentoGridDemo2.tsx
@@ -0,0 +1,61 @@
+import { BentoGrid, BentoGridItem } from "../ui/bento-grid";
+import {
+  IconArrowWaveRightUp,
+  IconBoxAlignRightFilled,
+  IconBoxAlignTopLeft,
+  IconSignature,
+  IconTableColumn,
+} from "@tabler/icons-react";
+
+export function BentoGridDemo2() {
+  return (
+    <BentoGrid className="max-w-4xl ml-4">
+      {items.map((item, i) => (
+        <BentoGridItem
+          key={i}
+          title={item.title}
+          description={item.description}
+          header={item.header}
+          icon={item.icon}
+          className={i === 3 || i === 6 ? "md:col-span-2" : ""}
+        />
+      ))}
+    </BentoGrid>
+  );
+}
+const Skeleton = () => (
+  <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
+);
+const items = [
+  {
+    title: "The Art of Design",
+    description: "Discover the beauty of thoughtful and functional design.",
+    header: <Skeleton />,
+    icon: <IconSignature className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "The Power of Communication",
+    description:
+      "Understand the impact of effective communication in our lives.",
+    header: <Skeleton />,
+    icon: <IconTableColumn className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "The Pursuit of Knowledge",
+    description: "Join the quest for understanding and enlightenment.",
+    header: <Skeleton />,
+    icon: <IconArrowWaveRightUp className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "The Joy of Creation",
+    description: "Experience the thrill of bringing ideas to life.",
+    header: <Skeleton />,
+    icon: <IconBoxAlignTopLeft className="h-4 w-4 text-neutral-500" />,
+  },
+  {
+    title: "The Spirit of Adventure",
+    description: "Embark on exciting journeys and thrilling discoveries.",
+    header: <Skeleton />,
+    icon: <IconBoxAlignRightFilled className="h-4 w-4 text-neutral-500" />,
+  },
+];
