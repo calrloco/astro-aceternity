885668c326c1a5e8f62632fc06d8199c749dbd71
carlo briganti <60385209+calrloco@users.noreply.github.com>
2024-06-14 14:25:36 +0200

    astor latest

--- src/components/GlowingStarsBackgroundCardPreview.tsx
+++ src/components/GlowingStarsBackgroundCardPreview.tsx
@@ -28,7 +28,7 @@ const Icon = () => {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      strokeWidth="1.5"
       stroke="currentColor"
       className="h-4 w-4 text-white stroke-2"
     >
